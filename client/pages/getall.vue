414a122d00a0118904aecb161916924f1b4f96e7
karel.paulik <karelpaulik@email.cz>
2023-12-06 22:33:01 +0100

    <b> to <strong>

--- client/pages/getall.vue
+++ client/pages/getall.vue
@@ -5,7 +5,7 @@
         <div>Id: {{ rec._id }}</div>
         <div>fName: {{ rec.fName }}</div>
         <div>lName: {{ rec.lName }}</div>
-        <div><b>Full name: </b> {{ `${rec.fName} ${rec.lName}` }}</div>
+        <div><strong>Full name: </strong> {{ `${rec.fName} ${rec.lName}` }}</div>
         <div v-if="rec.age">age: {{ rec.age }}</div>
         <!-- Následující platilo pro: na serveru: { raw: true } -->
         <!-- <div>city(parsed): {{ JSON.parse(rec.city) }}</div>
